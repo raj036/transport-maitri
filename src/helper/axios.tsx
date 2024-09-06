c3bae7d041670c6cbc45e50c8b5efd44adced7fe
Raj979-hub <raj.trivedi@maitriai.com>
2024-09-06 18:33:24 +0530

    asas

--- src/helper/axios.tsx
+++ src/helper/axios.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "https://795c-2405-201-37-21d9-c180-3d73-7560-2d07.ngrok-free.app/",
-  baseURL: "http://192.46.213.94:8001/", //fgfg
+  baseURL: "http://192.46.213.94:8009/", //fgfg
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
